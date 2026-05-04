d4391f758bff2557fc956e159eb82f9e474e15a0
Alexis Coates <youremail@example.com>
2026-05-04 16:35:32 -0400

    fix use client placement

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@
 import { useEffect, useState } from "react";
 import Link from "next/link";
 
-
 export default function Home() {
   const [books, setBooks] = useState<any[]>([]);
  const [selectedCategory, setSelectedCategory] = useState("All"); 
