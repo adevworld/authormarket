dadd189390ece444501678dba145d261709a2357
Alexis Coates <youremail@example.com>
2026-05-12 09:37:33 -0400

    fix featuredBooks

--- app/page.tsx
+++ app/page.tsx
@@ -130,7 +130,7 @@ export default async function Home() {
 
 console.log("BOOKS:", books);
 console.log("ERROR:", error);
-
+const featuredBooks = books?.filter((book) => book.featured) || [];
 
   return (
     <main
